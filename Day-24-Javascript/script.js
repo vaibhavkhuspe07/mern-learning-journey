3fc5ca0f7de2e274628f19c5fe470d40fc5af7bc
vaibhav_khuspe_07 <vaibhavkhuspe31@gmail.com>
2026-07-21 19:38:10 +0530

    Day 24: Introduction to JavaScript with Alerts, Console and Functions

---
+++ Day-24-Javascript/script.js
@@ -0,0 +1,12 @@
+
+console.log("Welcome to JavaScript!");
+
+alert("Welcome to Day 24 of JavaScript!");
+
+function showMessage() {
+
+    alert("Hello Vaibhav! Welcome to JavaScript.");
+
+    console.log("Button Clicked");
+
+}
